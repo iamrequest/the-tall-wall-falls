ae3b78ad03281a440b4006b35ead71ece37fb581
request <iamrequest@protonmail.com>
2021-02-15 16:22:05 -0500

    Sword angle is now editable

--- Assets/Scripts/Settings/AngleManager.cs
+++ Assets/Scripts/Settings/AngleManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,56 @@ using UnityEngine;
 /// 
 /// </summary>
 public class AngleManager : MonoBehaviour {
-    public float defaultAngle;
+    public SliderEventChannel sliderEventChannel;
+    private Quaternion defaultAngle;
+
+    public float offsetAngle;
     public enum Axis { X, Y, Z };
     public Axis targetAxis;
 
+    private void Awake() {
+        defaultAngle = transform.localRotation;
+    }
+    private void OnEnable() {
+        sliderEventChannel.onEventRaised += GetNewAngle;
+    }
+    private void OnDisable() {
+        sliderEventChannel.onEventRaised -= GetNewAngle;
+    }
 
+    private void FixedUpdate() {
+        SetAngle();
+    }
+
+    public float GetAngle() {
+        switch (targetAxis) {
+            case Axis.X:
+                return transform.rotation.x;
+            case Axis.Y:
+                return transform.rotation.y;
+            default:
+                return transform.rotation.z;
+        }
+    }
+
+    public void SetAngle() {
+        Vector3 rot = Vector3.zero; ;
+        switch (targetAxis) {
+            case Axis.X:
+                rot.x = offsetAngle;
+                break;
+            case Axis.Y:
+                rot.y = offsetAngle;
+                break;
+            default:
+                rot.z = offsetAngle;
+                break;
+        }
+
+        transform.localRotation = defaultAngle * Quaternion.Euler(rot);
+    }
+
+    private void GetNewAngle(float t, float newAngle) {
+        offsetAngle = newAngle;
+    }
 }
