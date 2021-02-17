99edd0f7d082f3b14b1f3273dbbb3eca41b9a211
request <iamrequest@protonmail.com>
2021-02-16 19:54:29 -0500

    Added coyote time to jumps

--- Assets/Scripts/VRPlayer/JumpManagerRB.cs
+++ Assets/Scripts/VRPlayer/JumpManagerRB.cs
@@ -16,8 +16,11 @@ public class JumpManagerRB : MonoBehaviour {
     [Range(0f, 10f)]
     public float jumpHeight;
     private bool jumpBuffered;
+    private int maxJumpCount = 1;
+    private int jumpCount;
 
     private int groundCount = 0, nonGroundCount = 0;
+
     public bool isTouching {
         get {
             return nonGroundCount > 0;
@@ -28,14 +31,24 @@ public class JumpManagerRB : MonoBehaviour {
             return groundCount > 0;
         }
     }
+    public bool isInCoyoteTime{
+        get {
+            return elapsedCoyoteTime < coyoteTime;
+        }
+    }
 
     [HideInInspector]
     // Non-normalized
-    private Vector3 groundNormal, nonGroundNormal;
+    private Vector3 groundNormal, nonGroundNormal, coyoteNormal;
 
     [Range(0f, 90f)]
     public float maxGroundAngle;
     private float minGroundDotProduct;
+
+    [Range(0f, 2f)]
+    [Tooltip("Extra delay before unsetting isGrounded")]
+    public float coyoteTime;
+    private float elapsedCoyoteTime;
 
     private void OnValidate() {
         minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
@@ -52,12 +65,34 @@ public class JumpManagerRB : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if (jumpBuffered && isTouchingGround) {
+        if (jumpBuffered && jumpCount < maxJumpCount) {
+            jumpCount++;
+
+            // Cache the normal for coyote time
+            coyoteNormal = GetJumpNormal();
+
             float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-            rb.velocity += GetJumpNormal() * jumpSpeed;
+            rb.velocity += coyoteNormal * jumpSpeed;
         }
 
+        // -- Coyote Time
+        // TODO: Need to decouple coyote time from isTouchingGround
+        if (isTouchingGround) {
+            elapsedCoyoteTime = 0f;
+            jumpCount = 0;
+        } else {
+            elapsedCoyoteTime += Time.fixedDeltaTime;
+        }
+
+        UnsetGroundCheck();
+
         jumpBuffered = false;
+    }
+
+    /// <summary>
+    /// Each physics frame we unset the calculated normals from the previous frame. During coyote time, we may want to delay this for a brief moment.
+    /// </summary>
+    private void UnsetGroundCheck() {
         groundCount = 0;
         nonGroundCount = 0;
         groundNormal = Vector3.zero;
@@ -69,18 +104,26 @@ public class JumpManagerRB : MonoBehaviour {
     public Vector3 GetJumpNormal() {
         if (isTouchingGround) {
             // -- Touching ground
+            // Normalize if there's more than 1 ground collider we're touching
             if (groundCount > 1) {
                 return groundNormal.normalized;
             } else {
                 return groundNormal;
             }
+
+        } else if (isInCoyoteTime) {
+            // If we're in the middle of coyote time, just use the normal that we cached from last frame
+            return coyoteNormal;
+
         } else if (isTouching) {
             // -- Not touching ground, but we're touching a wall
+            // Normalize if there's more than 1 collider we're touching
             if (nonGroundCount > 1) {
                 return groundNormal.normalized;
             } else {
                 return nonGroundNormal;
             }
+
         } else {
             // -- In air
             return Vector3.up;
