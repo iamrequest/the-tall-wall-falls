a3d4a618194c2aab01330fe317411397a3802c3a
request <iamrequest@protonmail.com>
2021-02-20 21:51:46 -0500

    Fixed tutorial signage input text
    - Also started working on sword health stuff, but ultimately scrapped the idea. Not enough time/energy

---
+++ Assets/Scripts/SwordDamageListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamageListener : MonoBehaviour {
+    private SwordManager swordManager;
+
+    private void Awake() {
+        swordManager = GetComponentInParent<SwordManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log(collision.collider);
+        // If the sword is no longer invincible
+        if (swordManager.elapsedSwordIFrames >= swordManager.swordIFrames) {
+                Debug.Log("Applying damage 1");
+            if(((1<<collision.collider.gameObject.layer) & swordManager.swordDamageLayers) == 0) {
+                Debug.Log("Applying damage 2");
+                swordManager.ApplyDamage();
+            }
+        }
+    }
+
+}
--- Assets/Scripts/SwordManager.cs
+++ Assets/Scripts/SwordManager.cs
@@ -7,6 +7,8 @@ public class SwordManager : MonoBehaviour {
     private PhysicsHand physicsHand;
     private AudioSource audioSource;
 
+    public GameStateEventChannel gameStateEventChannel;
+
     // -- Settings menu: Hide the sword/rope if the settings menu is open
     // Can't fire a projectile if the settings menu is open
     public BoolEventChannel settingsMenuOpenedChannel;
@@ -19,18 +21,40 @@ public class SwordManager : MonoBehaviour {
 
     public List<AudioClip> swordImpactSFX;
 
+    [Header("Sword Health")]
+    [Range(1, 10f)]
+    public int swordMaxHealth;
+    public int swordHealth;
+    public LayerMask swordDamageLayers;
+
+    [Range(0f, 5f)]
+    public float swordIFrames;
+    [HideInInspector]
+    public float elapsedSwordIFrames;
+
     private void Awake() {
         ropeProjectileManager = GetComponentInChildren<RopeProjectileManager>();
         physicsHand = GetComponentInParent<PhysicsHand>();
         audioSource = GetComponent<AudioSource>();
+
+        swordHealth = swordMaxHealth;
+        elapsedSwordIFrames = swordIFrames;
         SetSwordEnabled(true);
     }
     private void OnEnable() {
         settingsMenuOpenedChannel.onEventRaised += setIsSettingsMenuOpen;
+        gameStateEventChannel.onEventRaised += RepairSwordOnGameStart;
     }
 
     private void OnDisable() {
         settingsMenuOpenedChannel.onEventRaised -= setIsSettingsMenuOpen;
+        gameStateEventChannel.onEventRaised -= RepairSwordOnGameStart;
+    }
+
+    private void Update() {
+        if (elapsedSwordIFrames <= swordIFrames) {
+            elapsedSwordIFrames += Time.deltaTime;
+        }
     }
 
 
@@ -52,8 +76,13 @@ public class SwordManager : MonoBehaviour {
     /// </summary>
     /// <param name="isSwordEnabled"></param>
     public void SetSwordEnabled(bool isSwordEnabled) {
-        swordBlade.SetActive(isSwordEnabled);
-        swordBladeBroken.SetActive(isSwordEnabled);
+        if (isSwordEnabled) {
+            swordBlade.SetActive(swordHealth > 0);
+            swordBladeBroken.SetActive(swordHealth <= 0);
+        } else {
+            swordBlade.SetActive(false);
+            swordBladeBroken.SetActive(false);
+        }
         swordHandle.SetActive(isSwordEnabled);
 
         // This is a hacky workaround to fix a bug where the physics hand touch count gets out of sync when the settings menu gets opened/closed
@@ -67,5 +96,52 @@ public class SwordManager : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision) {
         audioSource.PlayOneShot(swordImpactSFX[Random.Range(0, swordImpactSFX.Count)]);
+
+        /*
+        // If the sword is no longer invincible
+        if (elapsedSwordIFrames >= swordIFrames) {
+            Debug.Log("Sword impact");
+            if(((1<<collision.collider.gameObject.layer) & swordDamageLayers) == 0) {
+                Debug.Log("Sword damage");
+                swordHealth--;
+                elapsedSwordIFrames = 0f;
+
+                if (swordHealth <= 0) {
+                    BreakSword();
+                }
+            }
+        }
+        */
+    }
+
+    public void BreakSword() {
+        swordBlade.SetActive(false);
+        swordBladeBroken.SetActive(true);
+
+        // TODO: Play SFX
+    }
+    public void RepairSword() {
+        swordBlade.SetActive(true);
+        swordBladeBroken.SetActive(false);
+
+        swordHealth = swordMaxHealth;
+        elapsedSwordIFrames = swordIFrames;
+
+        // TODO: Play SFX
+    }
+
+    private void RepairSwordOnGameStart(GameStateEventChannel.GameState gameState) {
+        if (gameState == GameStateEventChannel.GameState.STARTED) {
+            //RepairSword();
+        }
+    }
+
+    public void ApplyDamage() {
+        swordHealth--;
+        elapsedSwordIFrames = 0f;
+
+        if (swordHealth <= 0) {
+            //BreakSword();
+        }
     }
 }
